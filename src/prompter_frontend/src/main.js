42876b17b3b6f1b65ab98d48cbdb6e66f0da3b96
syaifudin21 <35588067+syaifudin21@users.noreply.github.com>
2024-08-23 13:24:12 +0800

    template chatgpt

--- src/prompter_frontend/src/main.js
+++ src/prompter_frontend/src/main.js
@@ -1,13 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import './index.css'
 
 import App from './App.vue'
-import { router } from './router'
+import router from './router'
+
+
+import './assets/main.css'
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
 
